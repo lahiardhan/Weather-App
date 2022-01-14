e8da5c4fffcd9a6b1dbafbf2e519ce62c7b204ce
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-01-14 09:03:54 +0700

    Final Result

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,34 +8,31 @@ const Form = ({ submitSearch }) => {
 
     const onSubmit = e => {
         e.preventDefault();
-        if (!location || location === '') {
-            return;
-        }
+        if (!location || location === '') return;
         submitSearch(location);
     };
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    aria-label="location"
-                    type="text"
-                    className={`${styles.input} form-control`}
-                    placeholder="Search for location"
-                    required
-                    value={location}
-                    onChange={e => setLocation(e.target.value)}
-                />
 
-                <button type="submit" className={styles.button} onClick={onSubmit}>
-                    SEARCH
-                </button>
-            </form>
-        </div>
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                aria-label="location"
+                type="text"
+                className={`${styles.input} form-control`}
+                placeholder="Search for location"
+                required
+                value={location}
+                onChange={e => setLocation(e.target.value)}
+            />
+
+            <button type="submit" className={styles.button} onClick={onSubmit}>
+                SEARCH
+            </button>
+        </form>
     );
 };
 
 Form.propTypes = {
-    submitSearch : PropTypes.func.isRequired,
-}
+    submitSearch: PropTypes.func.isRequired,
+};
 
 export default Form;
--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,36 +1,37 @@
 import React, { Fragment } from 'react';
-import styles from './Page.module.css';
 
 import Header from '../Header';
 import Form from '../Form';
-import Loader from '../Loader';
 import Error from '../Error';
-import Forecast from'../Forecast';
+import Loader from '../Loader';
+import Forecast from '../Forecast';
 
-import useForecast from "../../hooks/useForecast";
+import useForecast from '../../hooks/useForecast';
+
+import styles from './Page.module.css';
 
 const Page = () => {
     const { isError, isLoading, forecast, submitRequest } = useForecast();
 
-    const onSubmit = (value) => {
+    const onSubmit = value => {
         submitRequest(value);
-    }
+    };
+
     return (
         <Fragment>
             <Header />
-            <div className={styles.box}>
-                {/* Form */}
-                {!isLoading && <Form submitSearch={onSubmit}/>}
-
-                {/* Error */}
-                {isError && <Error message={isError}/> }
-                
-                {/* Loading */}
-                {isLoading && <Loader/> }
-                
-                {/* Forecast */}
-                {forecast && <Forecast/>}
-            </div>
+            {!forecast && (
+                <div className={`${styles.box} position-relative`}>
+                    {/* Form */}
+                    {!isLoading && <Form submitSearch={onSubmit} />}
+                    {/* Error */}
+                    {isError && <Error message={isError} />}
+                    {/* Loader */}
+                    {isLoading && <Loader />}
+                </div>
+            )}
+            {/* Forecast */}
+            {forecast && <Forecast forecast={forecast} />}
         </Fragment>
     );
 };
